7480126c29ade5dcbe8ed22ba8baa59575d51b91
Andre Karrlein <andre.karrlein@karrlein.com>
2019-12-09 11:45:22 +0100

    add coc settings json

--- coc_nvim_install.sh
+++ coc_nvim_install.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+if [ ! -x "$(command -v node)" ]; then
+    curl --fail -LSs https://install-node.now.sh/latest | sh
+    export PATH="/usr/local/bin/:$PATH"
+    # Or use apt-get
+    # sudo apt-get install nodejs
+fi
+
 nvim -c 'CocInstall -sync coc-json coc-html coc-phpls coc-css coc-flutter coc-markdownlint coc-vetur coc-yaml coc-xml|q'
+
+cp coc-settings.json ~/.config/nvim/.
+
+npm install coc-snippets --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
